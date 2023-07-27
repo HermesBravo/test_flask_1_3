b62f7c3af9b5ef3c48170ac40775eb3f5eb542ae
Hermes <131220852+HermesBravo@users.noreply.github.com>
2023-07-26 23:21:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, request
+app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return {"payload":"welcome to my project "}
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
