2b8a59aa25c64768e2632cf87d9d10528728152d
Josekaf123 <135049588+Josekaf123@users.noreply.github.com>
2023-07-31 22:12:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/init")
 def main():
     return {"payload":"welcome to my project "}
 
-
+@app.route('/read/:<content>', methods=['GET'])
+def read_content(content):
+    if content=="foo":
+        return {"payload":content}
+    else:
+        return "Usuario No Existe"
+    
+@app.route('/create/:<content>', methods=['GET'])
+def create_content(content):
+    if content=="bar":
+        return {"payload":content}
+    else:
+        return "Usuario No Existe"
 
 if __name__ == "__main__":
     app.run(debug=True)
