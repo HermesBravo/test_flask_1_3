250210cc0879b371af8f4088e44000572fce10cf
Ezequiel Zamora <zeke_dev@outlook.com>
2023-07-31 22:30:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask, request
-app = Flask(__name__)
+from flask import Flask, request, jsonify
 
-@app.route("/")
+app = Flask(name)
+
+@app.route("/init")
 def main():
-    return {"payload":"welcome to my project "}
+    return {"payload":"welcome to my project"}
 
+@app.route('/read/:<content>', methods=['GET'])
+def read_content(content):
+    if content=="foo":
+        return {"payload":content}
+    else:
+        return "Usuario No Existe"
+    
+@app.route('/create/:<content>', methods=['POST'])
+def create_content(content):
+    if content=="bar":
+        return {"payload":content}
+    else:
+        return "Usuario No Existe"
 
+@app.route('/delete/<content>', methods=['DELETE'])
+def delete_content(content):
+    if content == "qux":
+        return {"payload": content}
+    else:
+        return "Usuario No Existe"
 
-if __name__ == "__main__":
+@app.route('/put/<content>', methods=['PUT'])
+def put_content(content):
+    if content == "echo":
+        return {"payload": content}
+    else:
+        return "Usuario No Existe"
+
+if name == "main":
     app.run(debug=True)
